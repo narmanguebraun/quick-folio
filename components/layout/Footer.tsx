eb99854534651cda4fdd628b4ffdb95e2f829dd0
narmanguebraun <narmanguebraun1@gmail.com>
2024-01-30 19:20:53 +0100

    Update footer content and links

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,29 +4,44 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center mt-16 text-xs text-gray-400">
-      Made with
-      <Link
-        href="https://nextjs.org/"
-        className="pl-1 text-white hover:text-gray-400"
-      >
-        Next.js
-      </Link>
-      ,
-      <Link
-        href="https://tailwindcss.com/"
-        className="px-1 text-white hover:text-gray-400"
-      >
-        Tailwind CSS
-      </Link>
-      , and
-      <Link
-        href="https://github.com/"
-        className="pl-1 text-white hover:text-gray-400"
-      >
-        GitHub API
-      </Link>
-      .
+    <footer className=" text-center mt-16 text-xs text-gray-400">
+      <div>
+        <Link
+          href="https://github.com/narmanguebraun/quick-folio"
+          className="text-white hover:text-gray-400"
+        >
+          Quick-Folio
+        </Link>
+        , made with
+        <Link
+          href="https://nextjs.org/"
+          className="pl-1 text-white hover:text-gray-400"
+        >
+          Next.js
+        </Link>
+        ,
+        <Link
+          href="https://tailwindcss.com/"
+          className="pl-1 text-white hover:text-gray-400"
+        >
+          Tailwind CSS
+        </Link>
+        , and
+        <Link
+          href="https://github.com/"
+          className="pl-1 text-white hover:text-gray-400"
+        >
+          GitHub API
+        </Link>{" "}
+        by
+        <Link
+          href="https://github.com/narmanguebraun"
+          className="pl-1 text-white hover:text-gray-400"
+        >
+          Nina Armangué Braun
+        </Link>
+        .
+      </div>
     </footer>
   );
 };
