a5a18bb525869903d89d0ab95b8d6f88e1068b31
narmanguebraun <narmanguebraun1@gmail.com>
2024-01-30 18:46:52 +0100

    Small style fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import RepositoryList from "@/components/RepositoryList";
 export default async function Home() {
   return (
     <main>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <RepositoryList />
       </div>
     </main>
--- components/ui/RepoCard.tsx
+++ components/ui/RepoCard.tsx
@@ -22,7 +22,7 @@ export function RepoCard({
     <Link href={html_url}>
       <div className="bg-black p-4 min-h-[260px] rounded-lg border border-gray-700 group hover:border-white flex flex-col justify-between relative">
         <div>
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <h2 className="text-sm font-semibold">{name}</h2>
             <DateFormatter dateString={pushed_at} />
           </div>
