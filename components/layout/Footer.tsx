9bb2262512fb7773658b088ee8d1ce00eefdfe05
narmanguebraun <narmanguebraun1@gmail.com>
2024-01-30 19:56:27 +0100

    Small style fixes and edit footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,44 +4,43 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className=" text-center mt-16 text-xs text-gray-400">
-      <div>
-        <Link
-          href="https://github.com/narmanguebraun/quick-folio"
-          className="text-white hover:text-gray-400"
-        >
-          Quick-Folio
-        </Link>
-        , made with
-        <Link
-          href="https://nextjs.org/"
-          className="pl-1 text-white hover:text-gray-400"
-        >
-          Next.js
-        </Link>
-        ,
-        <Link
-          href="https://tailwindcss.com/"
-          className="pl-1 text-white hover:text-gray-400"
-        >
-          Tailwind CSS
-        </Link>
-        , and
-        <Link
-          href="https://github.com/"
-          className="pl-1 text-white hover:text-gray-400"
-        >
-          GitHub API
-        </Link>{" "}
-        by
-        <Link
-          href="https://github.com/narmanguebraun"
-          className="pl-1 text-white hover:text-gray-400"
-        >
-          Nina Armangué Braun
-        </Link>
-        .
-      </div>
+    <footer className="text-center mt-32 text-xs text-gray-400">
+      Made with
+      <Link
+        href="https://nextjs.org/"
+        className="pl-1 text-white hover:text-gray-400"
+      >
+        Next.js
+      </Link>
+      ,
+      <Link
+        href="https://tailwindcss.com/"
+        className="pl-1 text-white hover:text-gray-400"
+      >
+        Tailwind CSS
+      </Link>
+      , and
+      <Link
+        href="https://github.com/"
+        className="pl-1 text-white hover:text-gray-400"
+      >
+        GitHub API
+      </Link>
+      .<br></br>
+      Designed and coded by
+      <Link
+        href="https://github.com/narmanguebraun"
+        className="pl-1 text-white hover:text-gray-400"
+      >
+        Nina Armangué Braun
+      </Link>
+      .
+      <Link
+        href="https://github.com/narmanguebraun/quick-folio"
+        className="px-1 text-white hover:text-gray-400"
+      >
+        View source code &rarr;
+      </Link>
     </footer>
   );
 };
